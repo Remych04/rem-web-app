712e926d54550ccf5597a63c99084de2cf8b4532
remych04 <remych04@gmail.com>
2022-08-09 22:38:10 +0300

    start for bestiary

--- src/App.tsx
+++ src/App.tsx
@@ -1,75 +1,20 @@
-import React, { useState } from 'react';
-import {
-    AppShell,
-    Aside,
-    Footer,
-    Header,
-    MediaQuery,
-    Navbar,
-    useMantineTheme,
-    Text,
-    Burger,
-} from '@mantine/core';
+import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { Home } from './pages/home/home';
 import { Tasks } from './pages/tasks/tasks';
 import { Cv } from './pages/cv/cv';
-import { NavButtons } from './components/navbar/nav-buttons';
+import { W3Bestiary } from './pages/w3-bestiary';
 
 function App() {
-    const theme = useMantineTheme();
-    const [opened, setOpened] = useState(false);
     return (
-        <AppShell
-            styles={{
-                main: {
-                    background:
-                        theme.colorScheme === 'dark' ? theme.colors.dark[8] : theme.colors.gray[0],
-                },
-            }}
-            navbarOffsetBreakpoint="sm"
-            asideOffsetBreakpoint="sm"
-            navbar={
-                <Navbar p="md" hiddenBreakpoint="sm" hidden={!opened} width={{ sm: 200, lg: 300 }}>
-                    <NavButtons />
-                </Navbar>
-            }
-            aside={
-                <MediaQuery smallerThan="sm" styles={{ display: 'none' }}>
-                    <Aside p="md" hiddenBreakpoint="sm" width={{ sm: 200, lg: 300 }}>
-                        <Text>Application sidebar</Text>
-                    </Aside>
-                </MediaQuery>
-            }
-            footer={
-                <Footer height={60} p="md">
-                    Application footer
-                </Footer>
-            }
-            header={
-                <Header height={70} p="md">
-                    <div style={{ display: 'flex', alignItems: 'center', height: '100%' }}>
-                        <MediaQuery largerThan="sm" styles={{ display: 'none' }}>
-                            <Burger
-                                opened={opened}
-                                onClick={() => setOpened((o) => !o)}
-                                size="sm"
-                                color={theme.colors.gray[6]}
-                                mr="xl"
-                            />
-                        </MediaQuery>
-
-                        <Text>Application header</Text>
-                    </div>
-                </Header>
-            }
-        >
+        <React.Fragment>
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/tasks" element={<Tasks />} />
                 <Route path="/cv" element={<Cv />} />
+                <Route path="/w3-bestiary" element={<W3Bestiary />} />
             </Routes>
-        </AppShell>
+        </React.Fragment>
     );
 }
 
---
+++ src/pages/w3-bestiary/index.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export function W3Bestiary() {
+    return <div>w3 bestiary</div>;
+}

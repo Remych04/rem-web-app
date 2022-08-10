32f94ee456f6a0dca0107da68bcbf47dd1a66ff9
remych04 <remych04@gmail.com>
2022-08-10 23:53:31 +0300

    add styles and grid

--- src/components/monster-list/monster-card/MonsterCard.tsx
+++ src/components/monster-list/monster-card/MonsterCard.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import { Monster } from '../../../data/BestiaryDto';
 
+import styles from './index.module.css';
+
 type Props = {
     monster: Monster;
 };
 
 export function MonsterCard(props: Props) {
     return (
-        <React.Fragment>
-            <div>{props.monster.name}</div>
-        </React.Fragment>
+        <div className={styles.card}>
+            <div className={styles.container}>
+                <p>{props.monster.name}</p>
+            </div>
+        </div>
     );
 }
--- src/components/monster-list/monster-category/MonsterCategory.tsx
+++ src/components/monster-list/monster-category/MonsterCategory.tsx
@@ -2,17 +2,21 @@ import React from 'react';
 import { BestiaryCategory } from '../../../data/BestiaryDto';
 import { MonsterCard } from '../monster-card/MonsterCard';
 
+import styles from './index.module.css';
+
 type Props = {
     category: BestiaryCategory;
 };
 
 export function MonsterCategory(props: Props) {
     return (
-        <React.Fragment>
-            <div>{props.category.type}</div>
-            {props.category.monster_list.map((monster, index) => {
-                return <MonsterCard key={index} monster={monster} />;
-            })}
-        </React.Fragment>
+        <div className={styles.container}>
+            <h1 className={styles.header}>{props.category.type}</h1>
+            <div className={styles.list}>
+                {props.category.monster_list.map((monster, index) => {
+                    return <MonsterCard key={index} monster={monster} />;
+                })}
+            </div>
+        </div>
     );
 }

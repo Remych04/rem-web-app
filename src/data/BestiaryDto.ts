0207ac09073d371c0dcc3ebda7ac864c21aa2895
remych04 <remych04@gmail.com>
2022-08-10 10:29:08 +0300

    json and dto

---
+++ src/data/BestiaryDto.ts
@@ -0,0 +1,3 @@
+export interface BestiaryDto {
+    beasts: string[];
+}
--- src/pages/w3-bestiary/index.tsx
+++ src/pages/w3-bestiary/index.tsx
@@ -1,5 +1,16 @@
 import React from 'react';
 
+import bestiaryList from '../../mocks/bestiary-list.json';
+import { BestiaryDto } from '../../data/BestiaryDto';
+
 export function W3Bestiary() {
-    return <div>w3 bestiary</div>;
+    const bestiary: BestiaryDto = JSON.parse(JSON.stringify(bestiaryList));
+
+    return (
+        <React.Fragment>
+            {bestiary.beasts.map((beast, index) => {
+                return <div key={index}>{beast}</div>;
+            })}
+        </React.Fragment>
+    );
 }

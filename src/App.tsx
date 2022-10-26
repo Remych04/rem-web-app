8a2f55db64a19b668e0416e25c6d86e1d3c4abc7
remych04 <remych04@gmail.com>
2022-10-26 22:35:38 +0300

    change path, remove unused cv

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { Home } from './pages/home/home';
 import { Tasks } from './pages/tasks/tasks';
-import { Cv } from './pages/cv/cv';
 import { W3Bestiary } from './pages/w3-bestiary';
 
 function App() {
@@ -11,8 +10,7 @@ function App() {
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/tasks" element={<Tasks />} />
-                <Route path="/cv" element={<Cv />} />
-                <Route path="/w3-bestiary" element={<W3Bestiary />} />
+                <Route path="/w3" element={<W3Bestiary />} />
             </Routes>
         </React.Fragment>
     );
--- src/pages/cv/cv.tsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-export function Cv() {
-    return <div>There will be something about cv</div>;
-}
